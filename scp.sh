f3921b11174b778c6cbdac88a9b630715cdac211
Yi-love <438002896@qq.com>
2017-07-28 17:32:26 +0800

    travis gooooooooo

---
+++ scp.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+#History:
+#  2017-07-28 jin first release
+
+echo $FUTU_IM_KEY > $HOME/.ssh/id_rsa
+echo $FUTU_IM_PUB > $HOME/.ssh/id_rsa.pub
+
+zip -r ./futu.zip ./public
+
+scp ./futu.zip https://www.futu.im/
