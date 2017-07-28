22254094b82e64b2163fd62f857b2f8eb5b8e851
Yi-love <438002896@qq.com>
2017-07-28 20:50:40 +0800

    test travis

--- key.js
+++ key.js
@@ -5,9 +5,7 @@ const fs = require('fs');
 
 let basePath = process.argv[2];
 let rsecret = process.argv[3];
-let key = fs.readFileSync('./id_rsa.key');
-
-console.log('i got path: ' ,basePath);
+let key = fs.readFileSync('./id_rsa.key' , 'utf-8');
 
 let decipher = crypto.createDecipher('aes-256-cbc',rsecret);
 let dec = decipher.update(key,'hex','utf8');
