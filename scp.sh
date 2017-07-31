0763747544cb7de4f894497d467bc75b4fbc9d23
Yi-love <438002896@qq.com>
2017-07-31 20:05:24 +0800

    add key

--- scp.sh
+++ scp.sh
@@ -2,7 +2,7 @@
 
 #History:
 #  2017-07-28 jin first release
-unzip -fq  -P $FUTU_IM_ZIP_KEY ./id_rsa.zip -d $HOME/.ssh/
+unzip -q -P $FUTU_IM_ZIP_KEY ./id_rsa.zip -d $HOME/.ssh/
 echo $FUTU_IM_SING > $HOME/.ssh/known_hosts
 
 ls -a $HOME/.ssh/
