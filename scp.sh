c7efda0cb256d2b089bd55576d71873c785162db
Yi-love <438002896@qq.com>
2017-07-31 20:23:55 +0800

    add key

--- scp.sh
+++ scp.sh
@@ -2,11 +2,13 @@
 
 #History:
 #  2017-07-28 jin first release
+#  
 unzip -q -P $FUTU_IM_ZIP_KEY ./id_rsa.zip -d $HOME/.ssh/
-echo $FUTU_IM_SING > $HOME/.ssh/known_hosts
 
 chmod 600 $HOME/.ssh/id_rsa
 
-zip -r ./futu.zip ./public
+cd ./public
+zip -r ../futu.zip .
+cd ..
 
 scp ./futu.zip root@futu.im:/data/web/futu.im/pub
