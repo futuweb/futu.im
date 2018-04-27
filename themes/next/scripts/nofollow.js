f990c94dcd671510de27af1c2f2ee08580ef3527
TooBug <i@toobug.net>
2018-04-27 15:39:25 +0800

    SEO优化：禁止/tags收录，外链添加nofollow

---
+++ themes/next/scripts/nofollow.js
@@ -0,0 +1,7 @@
+hexo.extend.filter.register('after_render:html', (source) => {
+    console.log('after_render');
+    return source.replace(/rel="noopener"/g, function(str){
+        console.log('replace rel nofollow');
+        return 'rel="noopener external nofollow"';
+    });
+});
