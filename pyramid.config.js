7716c7f9dbbaa1b483ccf598f1da118f7b37ffae
Yi-love <438002896@qq.com>
2018-05-15 15:41:15 +0800

    禁止爬虫获取统计数据

--- pyramid.config.js
+++ pyramid.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 
 module.exports = {
+  bolgName:'富途Web开发团队',
   root: path.resolve(process.cwd() , 'source/_posts'), //bolg markdown file path
   url: 'https://futu.im/statistics/static',
   viewPath: path.resolve(process.cwd() , 'public/statistics'), //html path you want
