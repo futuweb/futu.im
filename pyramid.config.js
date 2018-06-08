98d6c95b14cf2156e1abd18a4b01b924209deab1
diandian <diandian@futunn.com>
2018-06-08 15:08:42 +0800

    头部微整形

--- pyramid.config.js
+++ pyramid.config.js
@@ -3,7 +3,7 @@
 const path = require('path');
 
 module.exports = {
-  blogName:'富途Web开发团队',
+  blogName:'富途WEB开发团队',
   root: path.resolve(process.cwd() , 'source/_posts'), //bolg markdown file path
   url: 'https://futu.im/statistics/static',
   viewPath: path.resolve(process.cwd() , 'public/statistics'), //html path you want
